dd59bb63be1689c6f9cf93a7f558a7caa33af392
CI010 <cijhn@hotmail.com>
2024-03-14 21:36:49 +0800

    add build script

---
+++ build.js
@@ -0,0 +1,58 @@
+const Database = require('better-sqlite3')
+const fs = require('fs')
+const buildNumber = process.env.BUILD_NUMBER
+
+const migrateScript = fs.readFileSync('./migrate.sql', 'utf-8')
+if (fs.existsSync('./build/db.sqlite')) {
+  fs.unlinkSync('./build/db.sqlite')
+}
+const db = new Database('./build/db.sqlite')
+db.exec(migrateScript)
+
+db.prepare('INSERT INTO metadata (version) VALUES (?)').run(buildNumber || 0)
+
+const files = fs.readdirSync('./files-v1')
+const mapping = {}
+const start = Date.now()
+for (const fileName of files) {
+  if (!fileName.endsWith('.json')) continue
+  const sha1 = fileName.slice(0, -5)
+  if (!sha1) continue
+  const content = JSON.parse(fs.readFileSync(`./files-v1/${fileName}`, 'utf-8'))
+  const insertFile = db.prepare('INSERT INTO file (sha1, name, domain) VALUES (?, ?, ?);')
+  const insertModrinth = db.prepare('INSERT INTO modrinth_version (sha1, project, version) VALUES (?, ?, ?);')
+  const insertCurseforge = db.prepare('INSERT INTO curseforge_file (sha1, project, file) VALUES (?, ?, ?);')
+  const insertForge = db.prepare('INSERT INTO forge_mod (sha1, id, version) VALUES (?, ?, ?);')
+  const insertFabric = db.prepare('INSERT OR IGNORE INTO fabric_mod (sha1, id, version) VALUES (?, ?, ?);')
+
+  try {
+    db.transaction((content) => {
+      insertFile.run(sha1, content.name, content.domain)
+      for (const modrinth of content.modrinth || []) {
+        insertModrinth.run(sha1, modrinth.projectId, modrinth.versionId)
+
+        if (content.curseforge) {
+          mapping[modrinth.projectId] = content.curseforge[0].projectId
+        }
+      }
+      for (const curseforge of content.curseforge || []) {
+        insertCurseforge.run(sha1, curseforge.projectId, curseforge.fileId)
+      }
+      for (const forge of content.forge || []) {
+        insertForge.run(sha1, forge.modId, forge.version ?? "")
+      }
+      for (const fabric of content.fabric || []) {
+        insertFabric.run(sha1, fabric.modId, fabric.version ?? "")
+      }
+    })(content)
+  } catch (e) {
+    console.error(content, e)
+  }
+}
+
+for (const [modrinth, curseforge] of Object.entries(mapping)) {
+  db.prepare('INSERT INTO project_mapping (curseforge_project, modrinth_project) VALUES (?, ?);')
+    .run(modrinth, curseforge)
+}
+
+console.log('Time:', Math.floor((Date.now() - start) / 1000))
--- migrate.sql
+++ migrate.sql
@@ -1,3 +1,7 @@
+CREATE TABLE metadata (
+    version INTEGER NOT NULL;
+);
+
 CREATE TABLE project_mapping (
     curseforge_project INTEGER NOT NULL,
     modrinth_project TEXT NOT NULL,
@@ -11,30 +15,30 @@ CREATE TABLE file (
 );
 
 CREATE TABLE modrinth_version (
-    sha1 TEXT PRIMARY KEY,
+    sha1 TEXT NOT NULL,
     project TEXT NOT NULL,
     version TEXT NOT NULL,
-    UNIQUE (project, version)
+    PRIMARY KEY (sha1, project, version)
 );
 
 CREATE TABLE curseforge_file (
-    sha1 TEXT PRIMARY KEY,
+    sha1 TEXT NOT NULL,
     project INTEGER NOT NULL,
     file INTEGER NOT NULL,
-    UNIQUE (project, file)
+    PRIMARY KEY (sha1, project, file)
 );
 
 CREATE TABLE forge_mod (
-    sha1 TEXT PRIMARY KEY,
+    sha1 TEXT NOT NULL,
     id TEXT NOT NULL,
     version TEXT NOT NULL,
-    UNIQUE (id, version)
+    PRIMARY KEY (sha1, id, version)
 );
 
 CREATE TABLE fabric_mod (
-    sha1 TEXT PRIMARY KEY,
+    sha1 TEXT NOT NULL,
     id TEXT NOT NULL,
     version TEXT NOT NULL,
-    UNIQUE (id, version)
+    PRIMARY KEY (sha1, id, version)
 );
 
