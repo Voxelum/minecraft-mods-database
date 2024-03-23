c99c2f8975e47d2d6b0012d6ac0c15c543501629
ci010 <cijhn@hotmail.com>
2024-03-23 09:22:41 -0700

    correct the project mapping and add mapping bin

--- build.js
+++ build.js
@@ -60,14 +60,48 @@ for (const fileName of files) {
 
 for (const [modrinth, curseforge] of Object.entries(mapping)) {
   db.prepare('INSERT INTO project_mapping (curseforge_project, modrinth_project) VALUES (?, ?);')
-    .run(modrinth, curseforge)
+    .run(curseforge, modrinth)
 }
 
 console.log('Time:', Math.floor((Date.now() - start) / 1000))
 const stat = fs.statSync(dbPath)
-console.log('Size:', Math.floor(stat.size / 1024 / 1024), 'MB')
+console.log('db.sqlite Size:', Math.floor(stat.size / 1024 / 1024), 'MB')
 const sha1 = require('crypto').createHash('sha1')
   .update(fs.readFileSync(dbPath))
   .digest('hex')
-console.log('SHA1:', sha1)
+console.log('db.sqlite SHA1:', sha1)
 fs.writeFileSync('./build/db.sqlite.sha1', sha1)
+
+const entries = Object.entries(mapping)
+const buffer = Buffer.alloc(entries.length * (8 + 4))
+for (let i = 0; i < entries.length; ++i) {
+  const [modrinth, curseforge] = entries[i]
+  writeBase58ToUint8Array(modrinth, buffer.slice(i * 12, i * 12 + 8))
+  write32BitsIntegersToUint8Array(curseforge, buffer.slice(i * 12 + 8, i * 12 + 12))
+}
+
+const writeBase58ToUint8Array = (base58Input, buffer) => {
+  // base58 input's length is 8
+  const alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+  for (let i = 0; i < 8; i++) {
+    const value = alphabet.indexOf(base58Input[i])
+    if (value === -1) {
+      throw new Error('Invalid base58 input')
+    }
+    buffer[i] = value
+  }
+}
+
+const write32BitsIntegersToUint8Array = (input, buffer) => {
+  for (let i = 0; i < 4; i++) {
+    buffer[i] = (input >> (i * 8)) & 0xff
+  }
+}
+
+fs.writeFileSync('./build/project_mapping.bin', buffer)
+console.log('project_mapping.bin Size:', Math.floor(buffer.length / 1024), 'KB')
+const sha1Bin = require('crypto').createHash('sha1')
+  .update(buffer)
+  .digest('hex')
+console.log('project_mapping.bin SHA1:', sha1Bin)
+fs.writeFileSync('./build/project_mapping.bin.sha1', sha1Bin)
